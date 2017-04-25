0cafc176b22926ff8f1f6f9504f5286e361276d9
audreylky <audrey@nextacademy.com>
2017-04-25 17:57:25 +0800

    1. Setup form 2. Input URL validation logic

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -6,4 +6,24 @@ class UrlsController < ApplicationController
 	def show
     	@url = Url.find(params[:id])
 	end
+
+	def new
+ 		@url = Url.new
+	end
+
+	def create
+  		@url = Url.new(url_params)
+  		@url.short_url = @url.short
+  		byebug
+  		if @url.save
+      		redirect_to @url  
+  		else
+     		render 'new'
+  		end   
+	end
+
+	private 
+	def url_params
+	    params.require(:url).permit(:long_url)
+	end
 end
